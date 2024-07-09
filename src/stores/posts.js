ccedc3a7dd7e67d359ad3cecf912b19759c15c61
deuspride <sinpride9@gmail.com>
2024-07-09 02:41:27 +0100

    adding http request with json-server

--- src/stores/posts.js
+++ src/stores/posts.js
@@ -4,48 +4,8 @@ export const usePostsStore = defineStore('posts-store', {
   //data
   state() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: 'This great book!',
-          body: 'Lorem ipsum corpus text',
-          author: 'John Doe',
-          created_at: '11/06/2023',
-          is_saved: false
-        },
-        {
-          id: 2,
-          title: 'This great book!',
-          body: 'Lorem ipsum corpus text',
-          author: 'John Doe',
-          created_at: '11/06/2023',
-          is_saved: false
-        },
-        {
-          id: 3,
-          title: 'This great book!',
-          body: 'Lorem ipsum corpus text',
-          author: 'John Doe',
-          created_at: '11/06/2023',
-          is_saved: false
-        },
-        {
-          id: 4,
-          title: 'This great book!',
-          body: 'Lorem ipsum corpus text',
-          author: 'John Doe',
-          created_at: '11/06/2023',
-          is_saved: false
-        },
-        {
-          id: 5,
-          title: 'This great book!',
-          body: 'Lorem ipsum corpus text',
-          author: 'John Doe',
-          created_at: '11/06/2023',
-          is_saved: false
-        }
-      ]
+      posts: [],
+      loading: true
     }
   },
   //computed
@@ -60,22 +20,49 @@ export const usePostsStore = defineStore('posts-store', {
   },
   //methods
   actions: {
+    getPosts() {
+      fetch('http://localhost:3000/posts')
+        .then((res) => res.json())
+        .then((data) => {
+          this.posts = data
+          this.loading = false
+        })
+        .catch((err) => {
+          this.errMsg = 'Something went wrong'
+          this.loading = false
+          console.log(err)
+        })
+    },
     addPost(post) {
-      this.posts.push({
+      const newPost = {
         id: this.posts.length + 1,
         title: post.title,
         body: post.body,
         author: 'Serval T@Nonoverse',
         created_at: new Date().toLocaleDateString(),
         is_saved: false
-      })
+      }
+      this.posts.push(newPost)
+      fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newPost)
+      }).catch()
     },
     deletePost(id) {
       this.posts = this.posts.filter((p) => p.id !== id)
+      fetch(`http://localhost:3000/posts/${id}`, {
+        method: 'DELETE'
+      }).catch()
     },
     savePost(id) {
       const post = this.posts.find((p) => p.id === id)
       post.is_saved = !post.is_saved
+      fetch(`http://localhost:3000/posts/${id}`, {
+        method: 'PATCH',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ is_saved: post.is_saved })
+      }).catch()
     }
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,18 +7,20 @@ import { usePostsStore } from '@/stores/posts'
 const postStore = usePostsStore()
 const postFilter = ref('All')
 
-const setPostFilter = () => {
-  postFilter.value = postFilter.value === 'All' ? 'Saved' : 'All'
-}
+const setPostFilter = () => (postFilter.value = postFilter.value === 'All' ? 'Saved' : 'All')
+
+postStore.getPosts()
 </script>
 
 <template>
   <div class="header">
     <div>
       <h2>{{ postFilter }} posts</h2>
+      <span v-show="postStore.loading" class="material-icons">cached</span>
     </div>
     <button @click="setPostFilter">Show {{ postFilter === 'All' ? 'saved' : 'all' }} posts</button>
   </div>
+  <div v-if="postStore.errMsg" class="error">{{ postStore.errMsg }}</div>
   <div v-if="postFilter === 'All'">
     <div v-for="post in postStore.sorted" :key="post.id">
       <MyWrapper>
@@ -43,15 +45,41 @@ const setPostFilter = () => {
   justify-content: space-between;
   background-color: #fff;
   box-shadow: 0px 1px 10px 1px #60606e31;
-  button {
-    background-color: #120eff;
-    color: aliceblue;
-    font-weight: 700;
-    padding: 10px 15px;
-    border-radius: 5px;
-    &:hover {
-      background-color: #254df2;
+  div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    span {
+      animation: spin 1s linear infinite;
     }
   }
+}
+button {
+  background-color: #120eff;
+  color: aliceblue;
+  font-weight: 700;
+  padding: 10px 15px;
+  border-radius: 5px;
+  &:hover {
+    background-color: #254df2;
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(-360deg);
+    }
+  }
+}
+.error {
+  background: #f87171;
+  color: #fff;
+  border-radius: 10px;
+  margin: 30px;
+  text-align: center;
+  padding: 1rem;
+  font-weight: 700;
+  font-size: 20px;
 }
 </style>
